5b1de9ae7bae004fdf14fab77da464d892a7d5ff
Tato <naranjosa2004@gmail.com>
2023-01-09 11:21:36 -0500

    Orden De Operaciones para Numeros en JavaScript

---
+++ 08_OrdenOperaciones.js
@@ -0,0 +1,25 @@
+// Orden De Las Operaciones
+
+let resultado;
+
+// Divisiones Y Multiplicaciones Tienen Mas Peso Que Sumas Y Restas
+
+resultado = (20 + 30) * 2;
+// console.log(resultado);
+
+/* Incrementos */
+
+let puntaje = 10;
+
+// Incremento en 1: ++
+
+puntaje++; //11
+++puntaje; //12
+
+// Decremento en 1: --
+puntaje--;
+
+//Incremento de mas de 1: +=
+
+puntaje +=10;
+console.log(puntaje);
