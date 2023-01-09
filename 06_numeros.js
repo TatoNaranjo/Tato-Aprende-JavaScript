96f3e6a8233cc5c270dd9831a6857a6119727d38
Tato <naranjosa2004@gmail.com>
2023-01-09 11:21:08 -0500

    Operaciones con Numeros en JavaScript

---
+++ 06_numeros.js
@@ -0,0 +1,23 @@
+// Operaciones Con Números
+
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 200.2;
+const numero4 = 0.102;
+const numero5 = -5;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+
+console.log(numero1 - numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero3 * numero4);
+
+/*Posibles Errores*/
+
+/*NaN = Not A Number*/
