0cefd255a134d057a3a84508f51d722cf969a249
Tato <naranjosa2004@gmail.com>
2023-01-09 11:21:18 -0500

    Libreria Math en JavaScript

---
+++ 07_Math.js
@@ -0,0 +1,35 @@
+// Objeto Math
+
+let resultado;
+
+//math Pi
+resultado = Math.PI;
+
+//Math Round: Redondear Un Numero
+resultado = Math.round(2.5);
+
+//Math Ceil: Siempre Redondea Un Numero Hacia Arriba
+resultado = Math.ceil(2.2);
+
+//Math Floor: Siempre Redondea Un Numero Hacia Abajo.
+resultado = Math.ceil(2.9);
+
+//Math sqrt: Raiz Cuadrada De Un Numero
+resultado = Math.sqrt(144);
+
+//Math Abs: Retorna El Valor Absoluto de Un Numero
+resultado = Math.abs(-180);
+
+//Math Min: En Base a Una Serie de Numeros Retorna el Menor de Todos
+resultado = Math.min(3, 2, 12, 1, 4, 66, 8, 1, 2, 4);
+
+// Math Max: En base a Una Serie de Numeros Retorna el Mayor de Todos
+resultado = Math.max(3, 2, 12, 1, 4, 66, 8, 1, 2, 4);
+
+//Math Random: Genera Un Numero Aleatorio Entre 0 y 1.
+resultado = Math.random();
+
+//Para Generar Un Numero Aleatorio Más Allá de 1
+resultado = Math.floor(Math.random() * 30);
+
+console.log(resultado);
