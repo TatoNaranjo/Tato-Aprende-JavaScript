2b511ee9aecabfebe816c549a816b735c66d782b
Tato <naranjosa2004@gmail.com>
2023-01-10 19:03:16 -0500

    Sintaxis Para Estructurar Strings de Forma Correcta en JavaScript

---
+++ 09_templateStrings.js
@@ -0,0 +1,13 @@
+const nombre = "Santiago";
+const email = "correo@correo.com";
+
+console.log(nombre);
+console.log(email);
+
+// console.log("Nombre Cliente: "+nombre+" \nEmail: "+email);
+
+/*Template Strings - Strings Literals*/
+
+
+//Sintaxis para colocar una variable ${}
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
