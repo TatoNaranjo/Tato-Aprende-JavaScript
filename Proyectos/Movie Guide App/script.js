168ec4fe828dfa95479553844337427eb49aa27d
Tato <naranjosa2004@gmail.com>
2023-02-06 13:09:14 -0500

    Movie Database App

--- Proyectos/Movie Guide App/script.js
+++ Proyectos/Movie Guide App/script.js
@@ -7,7 +7,7 @@ let result = document.getElementById("result");
 
 let getMovie = ()=>{
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+    let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
 
 
     /*Si el Campo Input está vacío */
